b17b03ebde09e2ace508ecf184a62053b8b8bc8f
_NSAKEY <root [at] abigisp [dot] com>
2015-05-25 18:17:11 -0500

    Renamed example.* to xmmp.* for consistency.

--- bootstrap.sh
+++ bootstrap.sh
@@ -44,8 +44,8 @@ service tor stop
 service prosody stop
 
 # SSL/TLS prep for prosody
-openssl genrsa -out /etc/prosody/certs/example.key 4096
-openssl req -new -key /etc/prosody/certs/example.key -out /etc/prosody/certs/example.crt
+openssl genrsa -out /etc/prosody/certs/xmpp.key 4096
+openssl req -new -key /etc/prosody/certs/xmpp.key -out /etc/prosody/certs/xmpp.crt
 openssl dhparam -out /etc/prosody/certs/dh-2048.pem 2048
 chown prosody:prosody /etc/prosody/certs/*
 chmod 600 /etc/prosody/certs/*
